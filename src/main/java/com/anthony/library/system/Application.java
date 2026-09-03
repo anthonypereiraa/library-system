fc48200366830e077bb44d291dd9cd3dac5c3f5e
Anthony Pereira <anthonypereiraa2006@gmail.com>
2026-09-03 16:25:37 -0300

    Removing some packages

--- src/main/java/com/anthony/library/system/Application.java
+++ src/main/java/com/anthony/library/system/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import static com.anthony.library.system.user.Role.*;
-
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class Application {
